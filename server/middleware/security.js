8fb2b210396412a9018edb6d6334be3e6cb4da62
CesarOsorioP <osoriocesar024@gmail.com>
2025-12-21 14:29:35 -0500

    url fixed

--- server/middleware/security.js
+++ server/middleware/security.js
@@ -12,7 +12,7 @@ export const corsOptions = {
     'http://localhost:5174',
     'http://localhost:5175',
     'http://localhost:3000',
-    'prestamos-juan-clone.vercel.app'
+    'https://prestamos-juan-clone.vercel.app'
   ].filter(Boolean),
   credentials: true,
   optionsSuccessStatus: 200
